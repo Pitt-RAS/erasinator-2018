3c35ca46b0c14c99776369a90e0265ed954ee8a1
Benjamin Rushin <benjaminprushin@gmail.com>
2018-11-16 11:36:40 -0500

    Changed tabs to spaces.

--- EdgeDetector.cpp
+++ EdgeDetector.cpp
@@ -2,25 +2,25 @@
 #include "EdgeDetector.h"
 
 EdgeDetector::EdgeDetector(){
-	qtra= QTRSensorsAnalog(IR_pins,(char)num_IR_sensors, IR_samples, 255);
-	pinMode(right_bumper_pin, INPUT_PULLUP);
-	pinMode(left_bumper_pin, INPUT_PULLUP);
-	edge_detected=false;
+    qtra= QTRSensorsAnalog(IR_pins,(char)num_IR_sensors, IR_samples, 255);
+    pinMode(right_bumper_pin, INPUT_PULLUP);
+    pinMode(left_bumper_pin, INPUT_PULLUP);
+    edge_detected=false;
 }
 void EdgeDetector::update(){
-	qtra.read(IR_values);
-	if(IR_values[0]>650 || IR_values[1]>650 || digitalRead(right_bumper_pin)==LOW || digitalRead(left_bumper_pin)==LOW){
+    qtra.read(IR_values);
+    if(IR_values[0]>650 || IR_values[1]>650 || digitalRead(right_bumper_pin)==LOW || digitalRead(left_bumper_pin)==LOW){
 //  if(digitalRead(right_bumper_pin)==LOW || digitalRead(left_bumper_pin)==LOW){
-		edge_detected=true;
-	}
-	else{
-		edge_detected=false;
-	}
+        edge_detected=true;
+    }
+    else{
+        edge_detected=false;
+    }
 }
 bool EdgeDetector::off_board(){
-	return edge_detected;
+    return edge_detected;
 }
 bool EdgeDetector::insta_off_board(){
-	update();
-	return edge_detected;
+    update();
+    return edge_detected;
 }
--- EdgeDetector.h
+++ EdgeDetector.h
@@ -6,23 +6,23 @@
 
 
 class EdgeDetector{
-	public:
-		EdgeDetector();
-		
-		void update();
-		bool off_board();
-		bool insta_off_board();
-		
-	private:
-		int right_bumper_pin=17;
-		int left_bumper_pin=16;
-		int right_IR_pinA=A0;
-		int left_IR_pinA=A1;
-		int num_IR_sensors=2;
-		int IR_samples=4;
+    public:
+        EdgeDetector();
+        
+        void update();
+        bool off_board();
+        bool insta_off_board();
+
+    private:
+        int right_bumper_pin=17;
+        int left_bumper_pin=16;
+        int right_IR_pinA=A0;
+        int left_IR_pinA=A1;
+        int num_IR_sensors=2;
+        int IR_samples=4;
     unsigned char IR_pins[2] = {right_IR_pinA,left_IR_pinA};
-		bool edge_detected;
-		unsigned int IR_values[2];
+        bool edge_detected;
+        unsigned int IR_values[2];
     QTRSensorsAnalog qtra;
 };
 
