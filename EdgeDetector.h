17e9abc5be4aae5d2f9a51752f34a5e2dc946b44
Benjamin Rushin <benjaminprushin@gmail.com>
2018-11-29 20:21:55 -0500

    Added test ino

--- EdgeDetector.h
+++ EdgeDetector.h
@@ -14,13 +14,13 @@ class EdgeDetector{
         bool insta_off_board();
 
     private:
-        int right_bumper_pin=17;
-        int left_bumper_pin=16;
-        int right_IR_pinA=A0;
-        int left_IR_pinA=A1;
+        unsigned char right_bumper_pin=17;
+        unsigned char left_bumper_pin=16;
+        unsigned char right_IR_pinA=A0;
+        unsigned char left_IR_pinA=A1;
         int num_IR_sensors=2;
         int IR_samples=4;
-    unsigned char IR_pins[2] = {right_IR_pinA,left_IR_pinA};
+        unsigned char IR_pins[2] = {right_IR_pinA,left_IR_pinA};
         bool edge_detected;
         unsigned int IR_values[2];
     QTRSensorsAnalog qtra;
