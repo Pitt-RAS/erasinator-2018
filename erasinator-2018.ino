888eba1cfc0e479086741ed4731e8d3d1cf91fc5
unknown <benjaminprushin@gmail.com>
2019-04-12 16:03:23 -0400

    Added methods to control PID class

--- erasinator-2018.ino
+++ erasinator-2018.ino
@@ -3,15 +3,36 @@
 Odometry odometry;
 
 MotorPID motorPid;
-
-
+int stepI;
+bool wasSteady = false;
 void setup() {
+  stepI = 0;
+  delay(1000);
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(13, OUTPUT);
-
+  motorPid.setMode(true);
+  motorPid.setVelocity(0);
+  motorPid.setHeading(-90);
 }
 
 void loop() {
- motorPid.update();
+  
+  motorPid.update();
+  if(motorPid.getSteady() && wasSteady==false){
+    stepI++;
+    
+  }
+  if(stepI==1){
+    motorPid.setMode(false);
+    motorPid.setDistance(100);
+  }
+  if(stepI==2){
+    motorPid.setMode(true);
+    motorPid.setHeading(-180);
+    
+  }
+  //Serial.printf("%d \n",stepI);
+
+  wasSteady=motorPid.getSteady();
 }
